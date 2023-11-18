119378d2dff35f52dd79007493580312d6455e66
NSQ <nsq263@gmail.com>
2023-11-17 21:58:10 -0700

    klfgsdujptfogujwerio0jhfi0w

--- backend/face_recogniser.py
+++ backend/face_recogniser.py
@@ -7,5 +7,11 @@ if __name__ == "__main__":
     while (1):
         ret, frame = video.read()
         cv2.imshow("frame", frame)
+
+        k = cv2.waitKey(1)
+        if (k%256 == 27):
+            break
+        
+        
     video.release()
     cv2.destroyAllWindows()
